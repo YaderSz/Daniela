2fd5d7cff9e63eb6ca8135552dfb75ea37bdcc0a
YaderSz <129681822+YaderSz@users.noreply.github.com>
2024-10-03 15:57:30 -0600

    Add files via upload

---
+++ Vehiculoss/Vehiculoss/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculoss
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 100;
+            string modelo = "KIA";
+            string temporada = "invierno";
+            Vehiculo vehiculo = new Vehiculo(modelo,price,temporada);
+            Console.WriteLine( $"Modelo: {vehiculo.Modelo}||" +
+                $"Precio: {vehiculo.Precio}||" +
+                $"PrecioDescuento: {vehiculo.result}");
+            Console.ReadLine();
+        }
+    }
+}
